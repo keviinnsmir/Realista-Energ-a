11a7b43334cca12233eb34ab570b86e9f41d71de
keviinnsmir <kevinquilumba026@gmail.com>
2025-04-29 17:00:46 +0200

    Add files via upload

--- script4.js
+++ script4.js
@@ -1,11 +1,16 @@
-// Menú hamburguesa
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const menu = document.querySelector(".menu");
-  
-    hamburger.addEventListener("click", function () {
-      menu.classList.toggle("active");
-      hamburger.classList.toggle("active");
-    });
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("menu");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
   });
-  
+});
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("mainNav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
