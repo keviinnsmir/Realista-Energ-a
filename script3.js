dcd57fc10cf824f0966aa5708ea91d9fe9ca9ce5
keviinnsmir <kevinquilumba026@gmail.com>
2025-05-14 14:20:02 +0200

    Add files via upload

--- script3.js
+++ script3.js
@@ -2,3 +2,10 @@ document.querySelector('.hamburger').addEventListener('click', function() {
     this.classList.toggle('open'); // Cambia el estado del icono hamburguesa
     document.querySelector('.menu').classList.toggle('active'); // Muestra/oculta el menú
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        // Realizar validaciones o enviar el formulario
+        // Si todo es correcto, el formulario será enviado
+    });
+});
